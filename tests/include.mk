88bdb716a5afef046b74f1e0f6182975c17481ba
benedictpaten <benedictpaten@gmail.com>
2012-06-30 20:55:44 +0100

    Fixing the tests

--- tests/include.mk
+++ tests/include.mk
@@ -1,5 +1,5 @@
 include ${rootPath}/include.mk
-haplotypeSequences=${dataPath}/assemblathon1/simulatedHaplotypes/testHap1.fa.complete ${dataPath}/assemblathon1/simulatedHaplotypes/testHap2.fa.complete ${dataPath}/assemblathon1/simulatedHaplotypes/testHap3.fa.complete
+haplotypeSequences=${dataPath}/assemblathon1/simulatedHaplotypes/Escherichia_coliO83_H1_1.trf.repmask.fa.complete ${dataPath}/assemblathon1/simulatedHaplotypes/Escherichia_coliO83_H1_1.trf.repmask.fa.complete ${dataPath}/assemblathon1/simulatedHaplotypes/Escherichia_coliO83_H1_1.trf.repmask.fa.complete
 newickTree=(((hapA1:0.002,hapA2:0.002):0.0000001,ecoli:0.002):0.000001,assembly:0.002);
 hap1EventString=hapA1
 hap2EventString=hapA2
